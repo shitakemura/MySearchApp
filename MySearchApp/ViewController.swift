5cc4a66d80ade3cd704072ea3f21fcc1cbd43251
shitakemura <shitakemura@gmail.com>
2017-02-19 15:36:13 +0900

    Initial Commit

---
+++ MySearchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySearchApp
+//
+//  Created by Shintaro Takemura on 2017/02/19.
+//  Copyright © 2017年 Shintaro Takemura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
